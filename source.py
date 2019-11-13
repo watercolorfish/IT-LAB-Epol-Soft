1252ab88eab9d6b9eaab0fd007a0a272e1c1b36f
Mozol Daria <orliet@yandex.ru>
2019-11-13 17:12:10 +0300

    Ready

--- source.py
+++ source.py
@@ -2,5 +2,19 @@ from sys import argv
 
 if __name__ == "__main__":
     script, first, second = argv
+    Arr1 = first[1:-1].split(',')
+    Arr2 = second[1:-1].split(',')
 
-    print(first)
+    outStr = ""
+    Ind = False
+    for i in Arr1:
+        for j in Arr2:
+            if (i==j):
+                Ind = True
+        if (Ind):
+            outStr+="1"
+        else:
+            outStr+="0"
+        Ind = False
+
+    print(outStr)
